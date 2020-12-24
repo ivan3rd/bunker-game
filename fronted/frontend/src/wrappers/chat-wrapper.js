279764c21d4f69f440a27e9ace1247d667dbbae7
ivan <ivantretiy@gmail.com>
2020-12-24 20:14:33 +0700

    changed functio chatWrapper into class

--- fronted/frontend/src/wrappers/chat-wrapper.js
+++ fronted/frontend/src/wrappers/chat-wrapper.js
@@ -1,17 +1,50 @@
-export function ChatWrapper(){
-    return(
+import {Component} from 'react'
+
+export class ChatWrapper extends Component{
+    constructor(props){
+        super(props);
+        this.state = {message:''}
+        
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event){
+        alert('Your message have been submited: ' +this.state.message)
+        event.preventDefault();
+    }
+
+    render(){
+        return(
         <div id="ChatWrapper">
             <div id='chatLog'>This is chat log
                 <ul>
-                    
+
                 </ul>
             </div>
 
-            <form id='chatForm'>
+            <form id='chatForm' onSubmit={this.handleSubmit}>
                 <input type='text' id="chatInput"autocomplete="off" title="chat"></input>
                 <input type='submit' value="Submit" />
             </form>
-        </div>
-           
-    )
+        </div> 
+        )
+    }
 }
+
+// export function ChatWrapper(){
+//     return(
+//         <div id="ChatWrapper">
+//             <div id='chatLog'>This is chat log
+//                 <ul>
+
+//                 </ul>
+//             </div>
+
+//             <form id='chatForm'>
+//                 <input type='text' id="chatInput"autocomplete="off" title="chat"></input>
+//                 <input type='submit' value="Submit" />
+//             </form>
+//         </div>
+           
+//     )
+// }
