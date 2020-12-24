06075b211f469ff2da34855a74b16a08eda1ec7b
ivan <ivantretiy@gmail.com>
2020-12-24 18:17:54 +0700

    added minimal version of socket server with express

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const app= require('express')()
+const httpserver = require('http').createServer(app)
+const io = require('socket.io')(httpserver,{
+    transports: ["websocket", "polling"]
+  })
+
+const port = 4000
+
+io.on('connection',socket=>{
+
+    //this block is for futer players connection monitoring
+    console.log(socket.id+' has been connected')
+    
+    socket.on('disconnect',(reason)=>{
+        console.log(socket.id+' has been DISconnected')
+        console.log(reason)
+    })
+
+})
+
+httpserver.listen(port,()=>{
+    console.log('lisening on port '+port)
+})
--- fronted/frontend/src/App.js
+++ fronted/frontend/src/App.js
@@ -1,10 +1,26 @@
 //import './App.css';
+
+
 import {OpponentsWrapper} from'./wrappers/oponents-wrapper';
 import {ChatWrapper} from './wrappers/chat-wrapper';
 import {PlayerStatsWrapper} from './wrappers/player-stats-wrapper';
+import io from 'socket.io-client';
+import { useEffect } from 'react';
 
+const socket = io.connect('http://localhost:4000',{
+  transports: ["websocket", "polling"]
+})
+
+socket.on('connect',()=>{
+  console.log('your socket id is ')
+})
 
 function App() {
+
+  useEffect(()=>{
+    console.log("This effect will accure every time something is rendered")
+    
+  })
 
   return (
     <div className="App">
