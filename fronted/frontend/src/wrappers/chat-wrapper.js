250f5df224042f1eb0418048f2a2dbe27adc7f04
ivan <ivantretiy@gmail.com>
2020-12-24 21:58:10 +0700

    finished basic form submit function

--- fronted/frontend/src/wrappers/chat-wrapper.js
+++ fronted/frontend/src/wrappers/chat-wrapper.js
@@ -1,38 +1,24 @@
-import {Component} from 'react'
+// import {Component} from 'react'
 
-export class ChatWrapper extends Component{
-    constructor(props){
-        super(props);
-        this.state = {message:''}
+// export class ChatWrapper extends Component{
+//     constructor(props){
+//         super(props);
+//         this.state = {message:''}
         
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+//         this.handleSubmit = this.handleSubmit.bind(this);
+//     }
 
-    handleSubmit(event){
-        alert('Your message have been submited: ' +this.state.message)
-        event.preventDefault();
-    }
+//     handleSubmit(event){
+//         alert('Your message have been submited: ' +this.state.message)
+//         event.preventDefault();
+//     }
+//     renderChat(){
+//         return chat
+//     }
 
-    render(){
-        return(
-        <div id="ChatWrapper">
-            <div id='chatLog'>This is chat log
-                <ul>
 
-                </ul>
-            </div>
-
-            <form id='chatForm' onSubmit={this.handleSubmit}>
-                <input type='text' id="chatInput"autocomplete="off" title="chat"></input>
-                <input type='submit' value="Submit" />
-            </form>
-        </div> 
-        )
-    }
-}
-
-// export function ChatWrapper(){
-//     return(
+//     render(){
+//         return(
 //         <div id="ChatWrapper">
 //             <div id='chatLog'>This is chat log
 //                 <ul>
@@ -40,11 +26,52 @@ export class ChatWrapper extends Component{
 //                 </ul>
 //             </div>
 
-//             <form id='chatForm'>
+//             <form id='chatForm' onSubmit={this.handleSubmit}>
 //                 <input type='text' id="chatInput"autocomplete="off" title="chat"></input>
 //                 <input type='submit' value="Submit" />
 //             </form>
-//         </div>
-           
-//     )
+//         </div> 
+//         )
+//     }
 // }
+
+import {useState,useEffect} from 'react'
+
+export function ChatWrapper(){
+    const [singleMsg,setSinglemsg]= useState("")
+    const [chatLog, setChatLog] = useState([])
+
+    const handleSubmit=(event)=>{
+        event.preventDefault();
+        alert('Your message have been submited.\nIt is a '+singleMsg)
+        //console.log(event)
+        //setChat([...chat, event.message])
+    }
+
+    const onTextChange=(props)=>{
+        //setSinglemsg(e.currentTarget.value);
+        props.target.value.setSinglemsg();
+    }
+
+    const renderChat =()=>{
+        //return chat.map()
+    }
+
+
+    return(
+        <div id="ChatWrapper">
+            <div id='chatLog'>This is chat log
+            </div>
+            <form id='chatForm' onSubmit={handleSubmit}>
+                <input 
+                type='text' 
+                id="chatInput"   
+                onChange={e => setSinglemsg(e.currentTarget.value) }
+                value={singleMsg} 
+                 />
+                <input type='submit' value="Submit" />
+            </form>
+         </div>
+    )
+
+}
